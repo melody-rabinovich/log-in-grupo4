5fb2b1974595bad0a700de6cd0554355dfde4b5a
aguskryss <aguskryss@gmail.com>
2022-09-07 21:46:02 -0300

    Funciona ahora

--- js/function.js
+++ js/function.js
@@ -1,3 +1,12 @@
+var datosUsuarios ={
+    usuario : '',
+    password: '',
+    nombre: '',
+    apellido: '',
+    edad: '',
+    profesion: ''
+
+}
 function abrirRegistro(){
     let div = document.getElementById('formRegistro');
     div.style.visibility = 'visible';
@@ -7,12 +16,66 @@ function abrirRegistro(){
 
 }
 
-function validarRegistro(){
-    let contraseña1 = document.getElementById('contraseñaRegistro').value;
-    let contraseña2 = document.getElementById('contraseña2Registro').value;
+function abrirLogin(){
+    let div = document.getElementById('formRegistro');
+    div.style.visibility = 'hidden';
 
-    if (contraseña1 != contraseña2){
-        // edad.style.color = 'red';
-        alert('Las contraseñas no coinciden');
+    let logIn = document.getElementById('logIn');
+    logIn.style.visibility = 'visible';
+
+}
+
+function validarUsuario(){
+    let usuario = document.getElementById('txtUsuario').value;
+    let password = document.getElementById('txtPassword').value;
+
+    if(usuario == ""){
+        document.getElementById('lblError').innerText = "No escribiste un usuario";
     }
+
+    if(password == ""){
+        document.getElementById('lblError').innerText = "No escribiste la contrasena";
+    }
+    if(datosUsuarios.usuario != usuario){
+        document.getElementById('lblError').innerText = "El usuario no es correcto";
+    }
+
+    if(datosUsuarios.password != password){
+        document.getElementById('lblError').innerText = "La contraseña no es correcta";
+    }
+
+    if(datosUsuarios.usuario == usuario && datosUsuarios.password == password){
+        //document.getElementById('txtUsuario').value = document.getElementById('txtPassword').value = document.getElementById('lblError') = '';
+        
+        alert('Bienvenido!! - ' + datosUsuarios.apellido + ', ' + datosUsuarios.nombre + ' (' + datosUsuarios.edad + ')'); 
+        document.getElementById('txtUsuario').value = '';
+        document.getElementById('txtPassword').value = '';
+        document.getElementById('lblError') = '';
+    }
+}
+
+function validarRegistro(){
+    console.log(datosUsuarios);
+    const signIn = document.ventana;
+    let usuario = signIn.usuario.value;
+    let password = signIn.contra.value;
+    let passwordRepeat = signIn.contraRepeat.value;
+    let nombre = signIn.nombre.value;
+    let apellido = signIn.apellido.value
+    let edad = signIn.edad.value;
+    let profesion = signIn.select.value;
+
+    if(password != passwordRepeat){
+        document.getElementById('lblErrorRegistro').innerText = "Las contrasenas no coinciden";
+        return false;
+    }
+
+    datosUsuarios.usuario = usuario;
+    datosUsuarios.nombre = nombre;
+    datosUsuarios.password = password;
+    datosUsuarios.edad = edad;
+    datosUsuarios.apellido = apellido;
+    datosUsuarios.profesion = profesion;
+
+    abrirLogin();
 }
